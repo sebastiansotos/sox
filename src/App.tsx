82b050cc8efc000b402763802e104c5f18b27de9
Sebastiansotos <sebastiansotogamerxd@gmail.com>
2026-04-01 11:33:14 -0400

    New Change

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const {} = useState()
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold text-center">Hello, Welcome to Sorex!</h1>
+      <h1 className="text-2xl font-bold text-center p-4">Hello, Welcome to Sorex!</h1>
       <p className="text-lg">La Mejor ropa de Venezuela, Representando  mi País </p>
       
     </div>
