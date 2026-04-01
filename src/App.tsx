1ab79da89a78bcfa5fa7ba05d9b9821f69229516
Sebastiansotos <sebastiansotogamerxd@gmail.com>
2026-04-01 11:50:41 -0400

    Style

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const {} = useState()
   return (
     <div className="p-4">
 <h1 style={{ fontStyle: 'italic', fontWeight: 'normal', textAlign: 'center' }}>
-  𝘏𝘦𝘭𝘭𝘰, 𝘞𝘦𝘭𝘤𝘰𝘮𝘦 𝘵𝘰 𝘚𝘰𝘳𝘦𝘹
+¡𝘏𝘦𝘭𝘭𝘰, 𝘞𝘦𝘭𝘤𝘰𝘮𝘦 𝘵𝘰 𝘚𝘰𝘳𝘦𝘹!
 </h1>
 
       <p className="text-lg">𝘓𝘢 𝘔𝘦𝘫𝘰𝘳 𝘳𝘰𝘱𝘢 𝘥𝘦 𝘝𝘦𝘯𝘦𝘻𝘶𝘦𝘭𝘢 🇻🇪, 𝘙𝘦𝘱𝘳𝘦𝘴𝘦𝘯𝘵𝘢𝘯𝘥𝘰 𝘮𝘪 𝘗𝘢í𝘴 </p>
