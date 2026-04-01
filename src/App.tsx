bd22df786e67893298f6e6ffbf3094e20ff2b6c9
Sebastiansotos <sebastiansotogamerxd@gmail.com>
2026-04-01 11:57:01 -0400

    Style Change

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react'
-
-
 function App() {
- 
- { React }
-const {} = useState()
-
-
   return (
-    <div className="p-4">
-<h1 style={{ fontStyle: 'italic', fontWeight: 'normal', textAlign: 'center' }}>
-¡𝘏𝘦𝘭𝘭𝘰, 𝘞𝘦𝘭𝘤𝘰𝘮𝘦 𝘵𝘰 𝘚𝘰𝘳𝘦𝘹!
-</h1>
-
-      <p className="text-lg">𝘓𝘢 𝘔𝘦𝘫𝘰𝘳 𝘳𝘰𝘱𝘢 𝘥𝘦 𝘝𝘦𝘯𝘦𝘻𝘶𝘦𝘭𝘢 🇻🇪, 𝘙𝘦𝘱𝘳𝘦𝘴𝘦𝘯𝘵𝘢𝘯𝘥𝘰 𝘮𝘪 𝘗𝘢í𝘴 </p>
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', // Alinea los elementos uno arriba del otro
+      alignItems: 'center',    // Centra horizontalmente
+      justifyContent: 'center', // Centra verticalmente en toda la pantalla
+      height: '100vh',         // Ocupa el 100% de la altura del navegador
+      textAlign: 'center'
+    }}>
+      {/* El texto se queda arriba */}
+      <h1>𝘓𝘢 𝘔𝘦𝘫𝘰𝘳 𝘳𝘰𝘱𝘢 𝘥𝘦 𝘝𝘦𝘯𝘦𝘻𝘶𝘦𝘭𝘢 🇻🇪</h1>
+      <p>𝘓𝘢 𝘔𝘦𝘫𝘰𝘳 𝘳𝘰𝘱𝘢 𝘥𝘦 𝘝𝘦𝘯𝘦𝘻𝘶𝘦𝘭𝘢 🇻🇪, 𝘙𝘦𝘱𝘳𝘦𝘴𝘦𝘯𝘵𝘢𝘯𝘥𝘰 𝘮𝘪 𝘗𝘢í𝘴</p>
       
+      {/* Tu logo en el medio */}
+      <img 
+        src="Logo.svg" 
+        alt="Logo Sorex" 
+        style={{ width: '200px', marginTop: '20px' }} 
+      />
     </div>
   )
 }
-
-export default App
