3e25b586143fdb5808a357915be0ef9c8a633ee3
Sebastiansotos <sebastiansotogamerxd@gmail.com>
2026-04-01 15:11:34 -0400

    the Change of Description

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         <div style={{ textAlign: 'center' }}>
           <h1 className="titulo-glitch">𝘏𝘦𝘭𝘭𝘰, 𝘞𝘦𝘭𝘤𝘰𝘮𝘦 𝘵𝘰 𝘚𝘰𝘳𝘦𝘹</h1>
           <p style={{ color: 'white', marginTop: '15px', fontSize: '1.4rem', opacity: 0.8 }}>
-            𝘓𝘢 𝘔𝘦𝘫𝘰𝘳 𝘳𝘰𝘱𝘢 𝘥𝘦 𝘝𝘦𝘯𝘦𝘻𝘶𝘦𝘭𝘢 
+             𝘋𝘪𝘴𝘦𝘯̃𝘰𝘴 𝘱𝘢𝘳𝘢 𝘲𝘶𝘪𝘦𝘯𝘦𝘴 𝘯𝘰 𝘣𝘶𝘴𝘤𝘢𝘯 𝘦𝘯𝘤𝘢𝘫𝘢𝘳, 𝘴𝘪𝘯𝘰 𝘥𝘰𝘮𝘪𝘯𝘢𝘳. 𝘚𝘵𝘰𝘤𝘬 𝘭𝘪𝘮𝘪𝘵𝘢𝘥𝘰, 𝘢𝘤𝘵𝘪𝘵𝘶𝘥 𝘪𝘯𝘧𝘪𝘯𝘪𝘵𝘢. 𝘚𝘪 𝘯𝘰 𝘷𝘢𝘴 𝘢 𝘱𝘰𝘳 𝘵𝘰𝘥𝘰, 𝘯𝘪 𝘭𝘰 𝘪𝘯𝘵𝘦𝘯𝘵𝘦𝘴. 𝘌𝘯𝘨𝘪𝘯𝘦𝘦𝘳𝘦𝘥 𝘧𝘰𝘳 𝘵𝘩𝘦 𝘦𝘭𝘪𝘵𝘦.
           </p>
         </div>
 
